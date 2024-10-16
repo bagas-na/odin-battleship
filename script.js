7a905d8eb70111fc236885e49671b0dad1bd2c76
bagas-na <bagasnurseno@gmail.com>
2024-10-16 21:30:25 +0700

    define ship class

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 "use strict";
 console.log("Hello via Bun!!");
+const SHIPTYPES = {
+    CARRIER: 5,
+    BATTLESHIP: 4,
+    CRUISER: 3,
+    SUBMARINE: 3,
+    DESTROYER: 2,
+};
+class Ship {
+    constructor(type) {
+        this.length = SHIPTYPES[type];
+        this.hitCount = 0;
+    }
+    hit() {
+        this.hitCount += 1;
+    }
+    isSunk() {
+        return this.hitCount >= this.length;
+    }
+}
+class Gameboard {
+}
+class Player {
+}
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,37 @@
 console.log("Hello via Bun!!");
+
+const SHIPTYPES = {
+  CARRIER: 5,
+  BATTLESHIP: 4,
+  CRUISER: 3,
+  SUBMARINE: 3,
+  DESTROYER: 2,
+}
+
+type ShipType = keyof typeof SHIPTYPES
+
+class Ship {
+  length: number;
+  hitCount: number;
+  
+  constructor(type: ShipType) {
+    this.length = SHIPTYPES[type];
+    this.hitCount = 0;
+  }
+
+  hit(): void {
+    this.hitCount += 1;
+  }
+
+  isSunk(): boolean {
+    return this.hitCount >= this.length
+  }
+}
+
+class Gameboard {
+
+}
+
+class Player {
+
+}
