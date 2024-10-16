b2709a25601c1daff3ca19913abf7a5f469c953a
bagas-na <bagasnurseno@gmail.com>
2024-10-16 21:13:04 +0700

    initialize project with tailwind css and typescript

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/script.ts",
+    "./src/**/*.{html,jsx,tsx}",
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
